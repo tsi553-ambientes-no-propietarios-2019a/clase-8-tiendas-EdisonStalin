f73e9f2432cb6f38c6975e643661be9883630fb9
EdisonStalin <edison.jumbo@epn.edu.ec>
2019-05-27 18:47:06 -0500

    base

--- tiendas.sql
+++ tiendas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 22-05-2019 a las 01:20:48
--- Versión del servidor: 10.1.34-MariaDB
--- Versión de PHP: 7.2.8
+-- Host: 127.0.0.1
+-- Generation Time: May 28, 2019 at 01:43 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `tiendas`
+-- Database: `tiendas`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `product`
+-- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
@@ -39,18 +39,24 @@ CREATE TABLE `product` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `product`
+-- Dumping data for table `product`
 --
 
 INSERT INTO `product` (`id`, `code`, `name`, `type`, `stock`, `price`, `user`) VALUES
 (1, 'p1', 'prod1', 'Alimento', 30, 3, 1),
 (2, 'p2', 'prod2', 'Vestimenta', 2, 2, 1),
-(3, 'p3', 'prod3', 'Salud', 3, 3, 1);
+(3, 'p3', 'prod3', 'Salud', 3, 3, 1),
+(4, '001', 'mazanas', 'Alimento', 30, 2, 2),
+(5, '002', 'Pera', 'Alimento', 10, 1, 2),
+(6, 'V001', 'Pantalon', 'Vestimenta', 8, 10, 2),
+(7, 'S001', 'Pastillas 5-B', 'Salud', 320, 3, 2),
+(8, 'T001', 'Camisa', 'Vestimenta', 5, 25, 3),
+(9, 'C021', 'Gorras', 'Vestimenta', 15, 5, 3);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -61,18 +67,20 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `store`, `username`, `password`) VALUES
-(1, 't1', 'u1', '4297f44b13955235245b2497399d7a93');
+(1, 't1', 'u1', '4297f44b13955235245b2497399d7a93'),
+(2, 't2', 'u2', '202cb962ac59075b964b07152d234b70'),
+(3, 't3', 'u3', '250cf8b51c773f3f8dc8b4be867a9a02');
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `product`
+-- Indexes for table `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`),
@@ -80,34 +88,34 @@ ALTER TABLE `product`
   ADD KEY `user` (`user`);
 
 --
--- Indices de la tabla `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `usuario unico` (`username`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `product`
+-- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de la tabla `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `product`
+-- Constraints for table `product`
 --
 ALTER TABLE `product`
   ADD CONSTRAINT `user` FOREIGN KEY (`user`) REFERENCES `user` (`id`);
